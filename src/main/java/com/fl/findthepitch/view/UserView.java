845cb561708110f85afcfb614217109fb789daea
fradevi02 <fra.devito02@gmail.com>
2025-02-11 11:29:54 +0100

    fix: modify settigns button

--- src/main/java/com/fl/findthepitch/view/UserView.java
+++ src/main/java/com/fl/findthepitch/view/UserView.java
@@ -1,21 +1,19 @@
 package com.fl.findthepitch.view;
 
 import com.fl.findthepitch.controller.SceneManager;
-import com.fl.findthepitch.controller.dbManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class UserView {
+
     @FXML
     private AnchorPane root;
 
@@ -28,12 +26,50 @@ public class UserView {
     @FXML
     private Button add;
 
+    @FXML
+    private VBox settingsMenu;
+
+    private boolean isSettingsMenuVisible = false;
+
+    @FXML
+    public void toggleSettingsMenu(ActionEvent event) {
+        isSettingsMenuVisible = !isSettingsMenuVisible;
+        settingsMenu.setVisible(isSettingsMenuVisible);
+
+        // Rotate the settings button when toggled
+        if (isSettingsMenuVisible) {
+            settings.setStyle("-fx-rotate: 90;");
+        } else {
+            settings.setStyle("-fx-rotate: 0;");
+        }
+    }
+
+    @FXML
+    public void goToAccount(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/accountView.fxml"));
+        AnchorPane accountRoot = loader.load();
+        Scene accountScene = new Scene(accountRoot);
+
+        // Push current scene to stack
+        SceneManager.pushScene(search.getScene());
+
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(accountScene);
+    }
+
+    @FXML
+    public void logout(ActionEvent event) {
+        System.out.println("Logging out...");
+        // Implement logout logic, such as clearing session data
+    }
+
+    @FXML
     public void goTonewMap(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/newMap.fxml"));
         AnchorPane newRoot = loader.load();
         Scene newScene = new Scene(newRoot);
 
-        //Push the current scene to the stack
+        // Push the current scene to the stack
         SceneManager.pushScene(search.getScene());
 
         Stage stage = (Stage) root.getScene().getWindow();
